991d41ec89bb2939b07875152a0717324538769e
rafah <136349907+rafah051@users.noreply.github.com>
2024-03-06 09:50:27 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,24 @@
+nomes = ['Rafa','Leo','Baiano','João','José']
+for nome in nomes:
+    print(nome)
+
+    print('-------')
+
+
+
+
+soma = 0
+for i in range(1,11):
+    soma = soma +1
+    print(soma)
+    print('-----------')
+
+
+
+
+n = 10
+fatorial = 1
+for i in range(1, n+1):
+    fatorial = fatorial * i
+print(f'O Fatorial de {n} é {fatorial}')
+    
